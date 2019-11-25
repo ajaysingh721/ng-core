b6aee53183459e141f1617a161bf8ddc619d3fd7
ajaysingh721 <ajay.singh721@gmail.com>
2019-11-25 13:43:21 +0530

    1 file modified

--- src/app/views/login/login.module.ts
+++ src/app/views/login/login.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from "@angular/core";
 
 import { SignInComponent } from "./sign-in/sign-in.component";
 import { SignOutComponent } from "./sign-out/sign-out.component";
+import { LoginRoutingModule } from "./login-routing.module";
 
 @NgModule({
-  declarations: [SignInComponent, SignOutComponent]
+  declarations: [SignInComponent, SignOutComponent],
+  imports: [LoginRoutingModule]
 })
 export class LoginModule {}
