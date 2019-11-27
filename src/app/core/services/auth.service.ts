63bc9f29896daa66e2ccb716946abe20333c0aab
ajaysingh721 <ajay.singh721@gmail.com>
2019-11-27 11:49:08 +0530

    1 file modified, 2 added

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,5 +2,13 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AuthService {
-  getToken(): string {}
+  getToken(): string {
+    return "";
+  }
+  isAuthenticated(): boolean {
+    return true;
+  }
+  hasAdminAccess(): boolean {
+    return false;
+  }
 }
