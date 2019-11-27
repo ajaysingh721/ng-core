bde05df68414d545ba37dcbee6f01fbb62b25c75
ajaysingh721 <ajay.singh721@gmail.com>
2019-11-27 12:25:29 +0530

    1 file added

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable()
+export class APIService {
+  apiSuffix: string;
+  constructor(apiSuffix: string, private http: HttpClient) {
+    this.apiSuffix = apiSuffix;
+  }
+
+  create(payload: any): any {
+    return this.http.post(`${this.apiSuffix}`, payload);
+  }
+
+  update(id: any, payload: any): any {
+    return this.http.put(`${this.apiSuffix}/${id}`, payload);
+  }
+
+  remove(id: any): any {
+    return this.http.delete(`${this.apiSuffix}/${id}`);
+  }
+  
+  getAll(): any {
+    return this.http.get(this.apiSuffix);
+  }
+
+  get(id: any): any {
+    return this.http.get(`${this.apiSuffix}/${id}`);
+  }
+}
