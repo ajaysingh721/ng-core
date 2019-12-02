ab0adcae359f09d1158871633c67d04f6a9898d3
ajaysingh721 <ajay.singh721@gmail.com>
2019-12-02 18:14:17 +0530

    3 files added

---
+++ src/app/shared/forms/forms.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IncidentComponent } from './incident/incident.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [IncidentComponent]
+})
+export class FormsModule { }
