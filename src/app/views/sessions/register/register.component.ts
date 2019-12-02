9e1c6132716bf19e7d0efd8cd737474566c9faa9
ajaysingh721 <ajay.singh721@gmail.com>
2019-12-02 12:38:42 +0530

    3 files modified

--- src/app/views/sessions/register/register.component.ts
+++ src/app/views/sessions/register/register.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatProgressBar, MatButton } from "@angular/material";
+import { Router } from "@angular/router";
+import { ThemeService } from "../../../core/services/theme.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  signupForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private themeService: ThemeService
+  ) {}
 
   ngOnInit() {
+    this.signuForm = this.fb.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required],
+      confirmPassword: ["", Validators.required]
+    });
   }
-
 }
