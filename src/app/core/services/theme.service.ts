6d8c896d73a6a2935013cffa6b3e23c0d2295b27
ajaysingh721 <ajay.singh721@gmail.com>
2019-12-02 11:48:12 +0530

    1 file modified

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -16,7 +16,7 @@ export class ThemeService {
       case "ng-core":
         this._theme.next({
           name: "Genpact",
-          theme: "hr-theme",
+          theme: "web-theme",
           logo:
             "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Genpact_logo.svg/1200px-Genpact_logo.svg.png",
           user: "https://image.flaticon.com/icons/svg/17/17004.svg"
