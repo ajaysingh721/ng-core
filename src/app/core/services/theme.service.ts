5dedd1183eb424759bab120ae3a9fde360731a16
ajaysingh721 <ajay.singh721@gmail.com>
2019-12-03 06:06:37 +0530

    1 file modified

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,13 +1,17 @@
 import { Injectable, Inject } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { DOCUMENT } from "@angular/common";
+import { OverlayContainer } from "@angular/cdk/overlay";
 
 @Injectable()
 export class ThemeService {
   private _theme: BehaviorSubject<any> = new BehaviorSubject<any>();
   theme$ = this._theme.asObservable();
 
-  constructor(@Inject(DOCUMENT) private document: Document) {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    overlayContainer: OverlayContainer
+  ) {
     const thirdLavelDomainName = this.document.location.origin
       .split("//")[1]
       .split(".")[0];
@@ -21,6 +25,7 @@ export class ThemeService {
             "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Genpact_logo.svg/1200px-Genpact_logo.svg.png",
           user: "https://image.flaticon.com/icons/svg/17/17004.svg"
         });
+        overlayContainer.getContainerElement().classList.add("web-theme");
         break;
       default:
         this._theme.next({
@@ -30,6 +35,7 @@ export class ThemeService {
             "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Genpact_logo.svg/1200px-Genpact_logo.svg.png",
           user: "https://image.flaticon.com/icons/svg/17/17004.svg"
         });
+        overlayContainer.getContainerElement().classList.add("hr-theme");
     }
   }
 }
