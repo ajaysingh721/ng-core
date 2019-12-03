1c3c38ccebe2d76ed2c92eb8c0115c8a5b733b80
ajaysingh721 <ajay.singh721@gmail.com>
2019-12-03 16:50:46 +0530

    1 file modified

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -23,13 +23,30 @@ export class ThemeService implements OnInit {
     console.log(thirdLavelDomainName);
     switch (thirdLavelDomainName) {
       case "ng-core":
-        this.apiService.get<Theme>(1).subscribe(res => {
-          //this._theme.next(res);
-          console.log(res);
+        this._theme.next({
+          id: 1,
+          name: "Genpact",
+          theme: "web-theme",
+          logo:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Genpact_logo.svg/1200px-Genpact_logo.svg.png",
+          user: "https://image.flaticon.com/icons/svg/17/17004.svg"
         });
         this.overlayContainer.getContainerElement().classList.add("web-theme");
         break;
+        this._theme.next({
+          id: 2,
+          name: "HeadStrong",
+          theme: "hr-theme",
+          logo:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Genpact_logo.svg/1200px-Genpact_logo.svg.png",
+          user: "https://image.flaticon.com/icons/svg/17/17004.svg"
+        });
+        // this.apiService.get<Theme>(1).subscribe(res => {
+        //   //this._theme.next(res);
+        //   console.log(res);
+        // });
         this.overlayContainer.getContainerElement().classList.add("hr-theme");
+        break;
     }
   }
 }
