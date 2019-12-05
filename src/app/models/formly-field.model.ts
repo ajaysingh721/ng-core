94a699b490a3f3b84f0fc8558b3fa7a68812535d
ajaysingh721 <ajay.singh721@gmail.com>
2019-12-05 17:22:41 +0530

    3 files modified, 2 added, 1 deleted

--- src/app/models/formly-field.model.ts
+++ src/app/models/formly-field.model.ts
@@ -1,18 +1,31 @@
+import { Expose } from "class-transformer";
+import { ExposedType } from "@nartc/automapper";
+
 export class FormlyField {
+  @Expose()
   key: string;
+  @Expose()
   type: string;
+  @ExposedType(() => TemplateOption)
   templateOptions: TemplateOption;
 }
 
 export class TemplateOption {
+  @Expose()
   label: string;
+  @Expose()
   placeholder: string;
+  @Expose()
   description: string;
+  @Expose()
   required: boolean;
+  @ExposedType(() => Option)
   options: Option[];
 }
 
 export class Option {
+  @Expose()
   value: string;
+  @Expose()
   lable: string;
 }
--- src/app/shared/pipes/to-formly-field.pipe.ts
+++ src/app/shared/pipes/to-formly-field.pipe.ts
@@ -1,65 +1,25 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { FormlyField, Option } from "../../models/formlyfield.model";
-import { Control, Item } from "../../models/control.model";
-import "automapper-ts";
+import { FormlyField } from "../../models/formly-field.model";
+import { Control } from "../../models/control.model";
+import { Mapper, ExposedType } from "@nartc/automapper";
+import "reflect-metadata";
 
 @Pipe({
   name: "toFormlyField"
 })
 export class ToFormlyFieldPipe implements PipeTransform {
   transform(value: Control, args?: any): any {
-    // mapping configuration
-    automapper
-      .createMap(Control, FormlyField)
-      // .forMember('fullName', (opts: AutoMapperJs.IMemberConfigurationOptions) => opts.mapFrom('name'))
-      .forMember("key", (opts: AutoMapperJs.IMemberConfigurationOptions) =>
-        opts.mapFrom("ControlName")
-      )
-      .forMember("type", (opts: AutoMapperJs.IMemberConfigurationOptions) =>
-        opts.mapFrom("ControlType")
-      )
-      .forMember(
-        "templateOptions.placeholder",
-        (opts: AutoMapperJs.IMemberConfigurationOptions) =>
-          opts.mapFrom("Placeholder")
-      )
-      .forMember(
-        "templateOptions.options",
-        (opts: AutoMapperJs.IMemberConfigurationOptions) =>
-          opts.mapFrom("ListItems")
-      )
-      .forMember(
-        "templateOptions.options",
-        (opts: AutoMapperJs.IMemberConfigurationOptions) =>
-          opts.mapFrom("ListItems.ItemLabel")
-      )
-      .forMember(
-        "templateOptions.options",
-        (opts: AutoMapperJs.IMemberConfigurationOptions) =>
-          opts.mapFrom("ListItems.ItemValue")
-      );
-    const result = automapper.map(Control, FormlyField, value);
+    Mapper.initialize(config => {
+      config.createMap(Control, FormlyField); // create a mapping from Control to FormlyField (one direction)
+      config
+        .createMap(Control, FormlyField)
+        .forMember(
+          destination => destination.templateOptions.options,
+          opts => opts.mapFrom(s => s.ListItems)
+        );
+    });
 
-    // automapper
-    //   .createMap(Item, Option)
-    //   // .forMember('fullName', (opts: AutoMapperJs.IMemberConfigurationOptions) => opts.mapFrom('name'))
-    //   .forMember("label", (opts: AutoMapperJs.IMemberConfigurationOptions) =>
-    //     opts.mapFrom("ItemLabel")
-    //   )
-    //   .forMember("vakue", (opts: AutoMapperJs.IMemberConfigurationOptions) =>
-    //     opts.mapFrom("ItemValue")
-    //   );
-
-    // const resultNew = automapper.map(
-    //   Item,
-    //   Option,
-    //   result.templateOptions.options
-    // );
-
-    // console.log(result.templateOptions);
-
-    // result.templateOptions["options"] = resultNew;
-
+    const result = Mapper.map(value, FormlyField);
     console.log(result);
     return result;
   }
