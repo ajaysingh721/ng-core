35eb3ae79e5ec9c19464befb5b38463a1db6dfd7
ajaysingh721 <ajay.singh721@gmail.com>
2019-12-16 11:53:09 +0530

    4 files modified

--- src/app/views/customer/customer-add/customer-add.component.ts
+++ src/app/views/customer/customer-add/customer-add.component.ts
@@ -1,5 +1,11 @@
 import { Control } from "./../../../models/control.model";
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  Renderer2
+} from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Observable } from "rxjs";
 import { FormlyFieldConfig } from "@ngx-formly/core";
@@ -17,11 +23,12 @@ export class CustomerAddComponent implements OnInit {
   customers: Observable<Customer[]>;
   formFields: FormlyFieldConfig[];
   formModels: { email: ""; password: "" };
-
+  @ViewChild("loadingBar", { static: false }) loadingBar: ElementRef;
   constructor(
     private apiService: APIService<Control>,
     private toFormlyField: ToFormlyFieldPipe,
-    private activeRoute: ActivatedRoute
+    private activeRoute: ActivatedRoute,
+    private renderer: Renderer2
   ) {
     //this.customers = store.pipe(select("customers"));
   }
@@ -34,6 +41,11 @@ export class CustomerAddComponent implements OnInit {
     this.apiService.endpoint = "api/forms";
     this.apiService.getAll().subscribe(res => {
       this.formFields = this.toFormlyField.transform(res);
+      this.renderer.setAttribute(
+        this.loadingBar.nativeElement,
+        "hidden",
+        "true"
+      );
     });
 
     this.activeRoute.queryParams.subscribe(params => {
